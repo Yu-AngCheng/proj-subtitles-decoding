7d7411b9403b273704b1b8b0345928e516adc674
Leon-Leyang <leyang_hu@brown.edu>
2023-12-10 02:36:23 -0500

    Move the func `load_and_resample` to `\utils\dataprocess.py`

--- models/audioencoder/audioencoder.py
+++ models/audioencoder/audioencoder.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
-import torchaudio
 from transformers import Wav2Vec2Processor, Wav2Vec2Model
+from utils.dataprocess import load_and_resample
 
 
 class AudioEncoder(nn.Module):
@@ -57,31 +57,6 @@ class AudioEncoder(nn.Module):
         self.model = self.model.to(device)
         self.device = device
         return self
-
-
-# TODO: move this to the custom dataset class
-def load_and_resample(file_path, target_sample_rate=16000):
-    """
-    Load the audio file and resample it to the target sample rate
-
-    Parameters:
-    - file_path (str): path to the audio file
-    - target_sample_rate (int): the target sample rate
-
-    Returns:
-    - waveform (np.ndarray): the waveform of the audio file
-    """
-    waveform, sample_rate = torchaudio.load(file_path)
-
-    # Convert stereo to mono
-    if waveform.shape[0] == 2:
-        waveform = waveform.mean(dim=0)
-
-    # Resample if necessary
-    if sample_rate != target_sample_rate:
-        resampler = torchaudio.transforms.Resample(orig_freq=sample_rate, new_freq=target_sample_rate)
-        waveform = resampler(waveform)
-    return waveform.numpy()
 
 
 if __name__ == "__main__":
