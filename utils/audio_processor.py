58061a2c42cbfdc7cc4778b28cc9a742e3020948
Leon-Leyang <leyang_hu@brown.edu>
2023-12-13 00:24:10 -0500

    Refactor `AudioProcessor` for the new data form

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -1,78 +1,79 @@
+import torch
 import torchaudio
 from transformers import Wav2Vec2Processor
 
 
 class AudioProcessor:
-    def __init__(self, target_sample_rate=16000, max_audio_length=64000):
+    def __init__(self, target_max_length, orig_sample_rate=44100, target_sample_rate=16000):
+        self.target_max_length = target_max_length
         self.target_sample_rate = target_sample_rate
-        self.max_audio_length = max_audio_length
+        self.orig_sample_rate = orig_sample_rate
 
         model_name = "jonatasgrosman/wav2vec2-large-xlsr-53-english"
         self.processor = Wav2Vec2Processor.from_pretrained(model_name)
 
-    def __call__(self, file_path):
+    def __call__(self, audio):
         """
-        Load the audio file, resample it to the target sample rate, and pad it or truncate it to the max_audio_length
+        Resample the audio, and pad it to the max_audio_length
 
         Parameters:
-        - file_path (str): path to the audio file
+        - audio (np.ndarray): A numpy array containing the audio
 
         Returns:
-        - padded_audio (torch.Tensor): A (1, max_audio_length) tensor containing the padded audio data
+        - padded_audio (torch.Tensor): A (max_audio_length,) tensor containing the padded audio
         """
-        audio_data = self._load_and_resample(file_path)
-        padded_audio = self._pad(audio_data)
+        audio = self._resample(audio)
+        padded_audio = self._pad(audio)
         return padded_audio
 
-    def _load_and_resample(self, file_path):
+    def _resample(self, audio):
         """
-        Load the audio file and resample it to the target sample rate
+        Resample the audio to the target sample rate
 
         Parameters:
-        - file_path (str): path to the audio file
-        - target_sample_rate (int): the target sample rate
+        - audio (np.ndarray): A numpy array containing the audio
 
         Returns:
         - waveform (np.ndarray): the waveform of the audio file
         """
-        waveform, sample_rate = torchaudio.load(file_path)
-
-        # Convert stereo to mono
-        if waveform.shape[0] == 2:
-            waveform = waveform.mean(dim=0)
-
-        # Resample if necessary
-        if sample_rate != self.target_sample_rate:
-            resampler = torchaudio.transforms.Resample(orig_freq=sample_rate, new_freq=self.target_sample_rate)
-            waveform = resampler(waveform)
+        audio = torch.tensor(audio, dtype=torch.float32)
+        resampler = torchaudio.transforms.Resample(orig_freq=self.orig_sample_rate, new_freq=self.target_sample_rate)
+        waveform = resampler(audio)
         return waveform.numpy()
 
-    def _pad(self, audio_data):
+    def _pad(self, audio):
         """
         Pad the audio data to the max_audio_length
 
         Parameters:
-        - audio_data (np.ndarray): A (audio_length) numpy array containing the audio data
+        - audio (np.ndarray): A numpy array containing the audio
 
         Returns:
         - padded_audio (torch.Tensor): A (max_audio_length) tensor containing the padded audio data
         """
-        padded_audio = self.processor(audio_data, padding="max_length", max_length=self.max_audio_length,
-                                      return_tensors="pt", sampling_rate=16000, truncation=True).input_values[0]
+        padded_audio = self.processor(audio, padding="max_length", max_length=self.target_max_length,
+                                      return_tensors="pt", sampling_rate=16000).input_values[0]
         return padded_audio
 
 
 if __name__ == "__main__":
-    import torch
+    import numpy as np
+    data_file = '../data/data_segmented.npy'
+    data = np.load(data_file, allow_pickle=True)
+    audios = data.item()['audio']
+    audio = audios[0]
+    print(f'The original audio data has shape {audio.shape}')
 
-    target_sample_rate = 16000
-    max_audio_length = 64000
-    audio_processor = AudioProcessor(target_sample_rate=target_sample_rate, max_audio_length=max_audio_length)
+    target_audio_sample_rate = 16000
+    orig_audio_sample_rate = 44100
 
-    audio_files = [r"D:\audio_1-3seconds\audio_1-3seconds\segment_1_00-01-24,935_00-01-26,111.mp3",
-                   r"D:\audio_1-3seconds\audio_1-3seconds\segment_2_00-01-26,996_00-01-28,728.mp3"]
+    orig_max_length = max([audio.shape[0] for audio in audios])
+    print(f'The original max length is {orig_max_length}')
 
-    audio_processor = AudioProcessor()
-    audio_data = [audio_processor(audio_file) for audio_file in audio_files]
-    audio_data = torch.stack(audio_data)
-    print(audio_data.shape)
+    target_max_length = int(orig_max_length * (target_audio_sample_rate / orig_audio_sample_rate))
+    print(f'The target max length is {target_max_length}')
+
+    audio_processor = AudioProcessor(target_sample_rate=target_audio_sample_rate, target_max_length=target_max_length)
+
+    processed_audio = audio_processor(audio)
+    print(f'The processed audio data has shape {processed_audio.shape}')
