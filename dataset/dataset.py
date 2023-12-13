49f8697b41f8f3fee798a0443e23665c3d6e9c17
Leon-Leyang <leyang_hu@brown.edu>
2023-12-13 01:44:13 -0500

    Refactor `CustomDataset` for the new data form

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,6 +1,4 @@
-import os
-import glob
-import re
+import numpy as np
 import torch
 from torch.utils.data import Dataset
 from utils.audio_processor import AudioProcessor
@@ -11,59 +9,73 @@ class CustomDataset(Dataset):
     Custom dataset for the audio and SEEG data
 
     Parameters:
-    - audio_dir (str): path to the directory containing the audio files
-    - seeg_dir (str): path to the directory containing the SEEG data
+    - data_file (str): path to the .npy file containing the data
     - train_ratio (float): ratio of the dataset to use for training
     - is_train (bool): whether to use the training or testing portion of the dataset
-    - audio_sample_rate (int): sample rate to use for the audio data
-    - audio_max_length (int): maximum length of the audio data(counted in audio_sample_rate)
+    - orig_audio_sample_rate (int): the original sample rate of the audio data
+    - target_audio_sample_rate (int): the target sample rate of the audio data
     """
-    def __init__(self, audio_dir, seeg_dir, train_ratio=0.8, is_train=True, audio_sample_rate=16000,
-                 audio_max_length=64000):
+    def __init__(self, data_file, train_ratio=0.8, is_train=True, orig_audio_sample_rate=44100,
+                 target_audio_sample_rate=16000):
         super(CustomDataset).__init__()
         self.is_train = is_train
-        self.audio_sample_rate = audio_sample_rate
-        self.audio_max_length = audio_max_length
+        self.orig_audio_sample_rate = orig_audio_sample_rate
+        self.target_audio_sample_rate = target_audio_sample_rate
 
-        self.audio_processor = AudioProcessor(target_sample_rate=self.audio_sample_rate, max_audio_length=self.audio_max_length)
+        # Load the data
+        data = np.load(data_file, allow_pickle=True)
+        self.audio_data = data.item()['audio']
+        self.seeg_data = data.item()['seeg']
 
-        # Get the number of files in the audio directory
-        pattern = os.path.join(audio_dir, "segment_*.mp3")
-        all_audi_file_paths = glob.glob(pattern)
-        # TODO: write a similar process for SEEG data
+        # Find the max length of the audio data in original sample rate
+        orig_audio_max_length = max([audio.shape[0] for audio in self.audio_data])
+
+        # Convert to the max length in the target sample rate
+        self.target_audio_max_length = int(orig_audio_max_length * (self.target_audio_sample_rate / self.orig_audio_sample_rate))
+
+        # Compute the max length of the sEEG data
+        self.seeg_max_length = max([seeg.shape[1] for seeg in self.seeg_data])
+
+        self.audio_processor = AudioProcessor(target_max_length=self.target_audio_max_length,
+                                              orig_sample_rate=self.orig_audio_sample_rate,
+                                              target_sample_rate=self.target_audio_sample_rate)
 
         # Split into train and test sets
-        # Extract indices and sort files
-        index_pattern = re.compile(r"segment_(\d+)_")
-        all_audi_file_paths.sort(key=lambda f: int(index_pattern.search(os.path.basename(f)).group(1)))
-        split_idx = int(len(all_audi_file_paths) * train_ratio)
+        split_idx = int(len(self.audio_data) * train_ratio)
         if self.is_train:
-            self.audio_file_paths = all_audi_file_paths[:split_idx]
+            self.audio_data = self.audio_data[:split_idx]
+            self.seeg_data = self.seeg_data[:split_idx]
         else:
-            self.audio_file_paths = all_audi_file_paths[split_idx:]
+            self.audio_data = self.audio_data[split_idx:]
+            self.seeg_data = self.seeg_data[split_idx:]
+
+        assert len(self.audio_data) == len(self.seeg_data), "The number of audio and sEEG data must be the same"
+
+        self.total_data_num = len(self.audio_data)
 
     def __getitem__(self, index):
         # Load and process the audio data
-        audio_path = self.audio_file_paths[index]
-        audio_data = self.audio_processor(audio_path)
+        audio = self.audio_data[index]
+        audio = self.audio_processor(audio)
 
         # Load the sEEG data
-        # TODO: Load the real SEEG data for this index
-        import random
-        seeg_data = torch.randn(4096, 57)
-        seeg_padding_mask = torch.zeros(4096, dtype=torch.bool)
-        # Randomly set a number of trialing positions to True to simulate padding
-        start_idx = random.randint(0, 4096)
-        seeg_padding_mask[start_idx:] = True
+        seeg = self.seeg_data[index]
 
-        return audio_data, seeg_data, seeg_padding_mask
+        # Create the torch boolean mask for the sEEG data
+        seeg_padding_mask = torch.zeros(self.seeg_max_length, dtype=torch.bool)
+        seeg_padding_mask[seeg.shape[1]:] = True
+
+        # Pad the sEEG data to the max length
+        seeg = torch.tensor(np.pad(seeg, ((0, 0), (0, self.seeg_max_length - seeg.shape[1])), 'constant',
+                                   constant_values=0))
+        return audio, seeg, seeg_padding_mask
 
     def __len__(self):
-        return len(self.audio_file_paths)
+        return self.total_data_num
 
 
 if __name__ == "__main__":
-    dataset = CustomDataset(audio_dir="../../audio_1-4seconds", seeg_dir=None, train_ratio=0.8, is_train=True)
+    dataset = CustomDataset(data_file='../data/data_segmented.npy', train_ratio=0.8, is_train=True)
     for i in range(10):
         audio_data, seeg_data, padding_mask = dataset[i]
         print(audio_data.shape, seeg_data.shape, padding_mask.shape)
